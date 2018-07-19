9c60d46d119715894d2f7a189de1a99e9f08c7ea
Darren Young <dlyoung5@asu.edu>
2018-07-19 14:13:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments){
   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   for (let musicians = 0, let instruments = 0; musicians <= 3, instruments <= 3; musicians++, instruments++){
-    return theBeatlesPlay(musicians + " plays the " + instruments)
+    return theBeatlesPlay(`${musicians} + plays the + ${instruments}`)
   }
   return theBeatlesPlay;
 }
